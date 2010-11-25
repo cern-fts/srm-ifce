5a81a25eff2695ba8c01a297811aceff6ed3e1ca
Todor Manev <Todor.Manev@cern.ch>
2010-11-25 15:58:23 +0000

    Removed lib from the script.

--- test/execute_tests.sh
+++ test/execute_tests.sh
@@ -30,7 +30,7 @@ fi
 echo $LD_LIBRARY_PATH
 echo $LIBDIR
 if [ -n "$1" ]; then
-	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$1/$LIB_FOLDER_TYPE
+	export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$1
 fi
 echo $LD_LIBRARY_PATH
 echo $LIBDIR
