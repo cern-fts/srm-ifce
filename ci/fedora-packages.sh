0f4db868226b6241d31e11884bd7408610e6e8b3
Joao Lopes <joao.pedro.batista.lopes@cern.ch>
2021-06-28 14:06:24 +0200

    CI: Publish stage

--- ci/fedora-packages.sh
+++ ci/fedora-packages.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ -f /usr/bin/dnf ]]; then
   dnf install -y dnf-plugins-core git rpm-build which tree python2 \
-                 cmake cmake3 make gcc gcc-c++
+                 cmake make gcc gcc-c++
 else
   yum install -y yum-utils git rpm-build make which tree python2 \
                  cmake cmake3 make gcc gcc-c++
